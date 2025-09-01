296c0fb3132e391e6139a8a4cd63833557da64b7
G4PZ <giancarlospagadorz@gmail.com>
2025-09-01 00:07:16 -0500

    Fix color variable names and update button styles in the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,29 +7,27 @@ export default function Page() {
     <div className="min-h-screen bg-white">
 
         <div className=" containter bg-Skyblue w-full flex justify-between p-2 h-[66px] border-b border-azulBorde items-center">
-
           <div className="flex items-center px-[22px] gap-2">
             <FaStethoscope className="text-blackBlue" size={20}/>
-            <span className=" font-semibold p-3 text-lg text-blackBlue">Maternia Care</span>
+            <span className=" font-semibold p-3 text-lg text-blackBluee">Maternia Care</span>
           </div>
 
-
           <div className="flex gap-5">
-            <a href="#" className="px-3 py-1 rounded-md bg-sky-400 text-white font-semibold">
+            <a href="#" className="px-3 py-1 rounded-md bg-FondoAzul text-white font-semibold">
               Home
             </a>
-            <a href="#" className="px-3 py-1 rounded-md text-sky-900 ">
+            <a href="#" className="px-3 py-1 rounded-md text-blackBluee ">
               Services
             </a>
-            <a href="#" className="px-3 py-1 rounded-md text-sky-900 ">
+            <a href="#" className="px-3 py-1 rounded-md text-blackBluee ">
               Blog
             </a>
-            <a href="#" className="px-3 py-1 rounded-md text-sky-900 ">
+            <a href="#" className="px-3 py-1 rounded-md text-blackBluee">
               Contact
             </a>
           </div>        
-          
         </div>
+        
          <div>
         
         </div>
