c575ace539693777150d43671af999fa9a7ccf2e
laszlo-caballero <laszlocaballero@gmail.com>
2025-09-11 00:42:00 -0500

    Actualizar el componente CardMap para utilizar la posición geográfica correcta y mejorar la gestión de la cámara en el mapa.

--- src/components/ui/card-services/CardMap.tsx
+++ src/components/ui/card-services/CardMap.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   APIProvider,
   Map,
@@ -8,13 +8,37 @@ import {
 } from "@vis.gl/react-google-maps";
 
 export default function CardMap() {
-  const position = { lat: 53.54, lng: 10 };
+  //-8.105179120912057, -79.03624806377229
+  const position = { lat: -8.105179120912057, lng: -79.03624806377229 };
   const [open, setOpen] = useState(false);
+  const [camara, setCamara] = useState({
+    center: { lat: -8.105179120912057, lng: -79.03624806377229 },
+    zoom: 15,
+  });
+
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     setCamara({
+  //       center: {
+  //         lat: position.coords.latitude,
+  //         lng: position.coords.longitude,
+  //       },
+  //       zoom: 15,
+  //     });
+  //   });
+  // }, []);
 
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_API_KEY || ""}>
       <div className="w-full h-[270px]">
-        <Map zoom={9} center={position} mapId="e4ce84c3f2e1437a">
+        <Map
+          zoom={camara.zoom}
+          center={camara.center}
+          onCameraChanged={(e) => {
+            setCamara(e.detail);
+          }}
+          mapId="e4ce84c3f2e1437a"
+        >
           <AdvancedMarker position={position} onClick={() => setOpen(true)}>
             <Pin background={"red"} borderColor={"red"} glyphColor={"red"} />
           </AdvancedMarker>
